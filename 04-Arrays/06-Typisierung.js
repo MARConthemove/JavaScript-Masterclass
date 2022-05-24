2c6828c1c326a874067d7890f0713c5cabf02b27
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-05-24 17:40:12 +0200

    Array section completed

---
+++ 04-Arrays/06-Typisierung.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/**
+ * In JavaScript sind Arrays nicht typisiert
+ * Auch verschachtelte Array sind zulässig!
+ */
+let liste = ['Banane', 4, ['Element 1', 'Element 2']]
+
+console.log(liste[2][0])
