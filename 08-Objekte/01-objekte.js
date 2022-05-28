8ceb8e4a1d0ebd6d23e13af628900e43a9337914
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-05-28 19:44:34 +0200

    objects in javascript

--- 08-Objekte/01-objekte.js
+++ 08-Objekte/01-objekte.js
@@ -1,4 +1,11 @@
 "use strict"
 
-const student = "Max Mustermann"
+const student = {
+    name: "Max Mustermann",
+    age: 21,
+    language: "Englisch"
+}
 
+console.log(student.name)
+console.log(student.age)
+console.log(student.language)
