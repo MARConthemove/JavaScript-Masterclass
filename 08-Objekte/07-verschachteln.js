cc6f810d12aeaeb9e4254fe50adb399f47432935
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-01 18:33:50 +0200

    nesting of object with different data types

---
+++ 08-Objekte/07-verschachteln.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const students = [
+    { firstname: "Max", lastname: "Mustermann" },
+    { firstname: "Erika", lastname: "Mustermann" }
+]
+
+console.log("students:", students)
+
+for (const student of students) {
+    console.log("Student: ", student)
+}
+
+const obj = {
+    items: ["Apfel", "Orange"]
+}
+
+console.log("obj: ", obj)
+
