1434f32e1cf71d9d9ae60fdfa5dc8bbc953deb12
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-03 16:56:13 +0200

    challenge of DOM manipulation solved

--- 13-JavaScript im Browser (2)/Projekt - Aufgabe/js/main.js
+++ 13-JavaScript im Browser (2)/Projekt - Aufgabe/js/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     for (const helpElement of helpElements) {
         helpElement.addEventListener("click", (event) => {
             event.preventDefault()
+            event.stopww()
 
             const helpText = helpElement.attributes["data-help"].value
             alert(helpText)
@@ -14,13 +15,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const cardHeaders = document.querySelectorAll(".card-header")
 
+    // Iteration over cardHeaders:
     for (const cardHeader of cardHeaders) {
+        // Correct Autocomplete (so that cardHeader is known as an HTML Element)
         if (cardHeader instanceof HTMLElement) {
+            // Selector Variablen:
+            const cardBody = cardHeader.nextElementSibling
+            const cardHeaderIcon = cardHeader.querySelector("i")
+
+            // Initial State of Page:
+            cardBody.classList.add("d-none")
+            cardHeaderIcon.classList.remove("fa-angle-double-down")
+            cardHeaderIcon.classList.add("fa-angle-double-right")
+
+            // Mouseover Cursor anzeigen
+            cardHeader.style.cursor = "pointer"
+
+            // Click EventListener:
             cardHeader.addEventListener("click", (event) => {
                 event.preventDefault()
 
-                const cardBody = cardHeader.nextElementSibling
+                // Open or Close CardBody:
                 cardBody.classList.toggle("d-none")
+
+                // Change Icon when CardBody opened or closed:
+                if (cardBody.classList.contains("d-none")) {
+                    cardHeaderIcon.classList.remove("fa-angle-double-down")
+                    cardHeaderIcon.classList.add("fa-angle-double-right")
+                } else {
+                    cardHeaderIcon.classList.remove("fa-angle-double-right")
+                    cardHeaderIcon.classList.add("fa-angle-double-down")
+                }
             })
         }
     }
