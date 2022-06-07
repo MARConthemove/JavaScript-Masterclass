fdce1d88264f12350a9786a9ca26c6ed4ef35b9e
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-07 10:05:28 +0200

    Protype problem

---
+++ 19-Prototype/01-problem.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const studentsGetName = function () { return this.firstname + " " + this.lastname }
+
+const students = [
+    {
+        firstname: "Max",
+        lastname: "Müller",
+        getName: studentsGetName
+    },
+    {
+        firstname: "Erika",
+        lastname: "Mustermann",
+        getName: studentsGetName
+    },
+]
+
+for (const student of students) {
+    console.log(student.getName())
+}
