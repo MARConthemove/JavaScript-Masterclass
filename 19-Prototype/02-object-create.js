bbcc049f47757171ebb567f0eff7c68d6b649bcc
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-07 15:06:58 +0200

    small changes

--- 19-Prototype/02-object-create.js
+++ 19-Prototype/02-object-create.js
@@ -1,36 +1,36 @@
-"use strict"
+'use strict'
 
 // const student = {firstname: "Max", lastname: "Müller", subjects: []}
 
 const student = Object.create(Object.prototype, {
     firstname: {
-        value: "Max",       // Der initialie Wert
-        writable: false,     // Darf dieser Wert überschrieben werden?
-        // Beispiel: student.firstname = "Maximilian"
-        configurable: false, // Darf diese Eigenschaft per "delete" gelöscht werden?
-        // Beispiel: delete student.firstname
-        enumerable: true    // Taucht diese Eigenschaft in Schleifen auf?
-        // for (const key in student) { console.log(key) }
+        value: 'Max',               // Der initialie Wert
+        writable: false,            // Darf dieser Wert überschrieben werden?
+                                    // Beispiel: student.firstname = "Maximilian"
+        configurable: false,        // Darf diese Eigenschaft per "delete" gelöscht werden?
+                                    // Beispiel: delete student.firstname
+        enumerable: true,           // Taucht diese Eigenschaft in Schleifen auf?
+                                    // for (const key in student) { console.log(key) }
     },
     lastname: {
-        value: "Müller",
+        value: 'Müller',
         writable: true,
         configurable: true,
-        enumerable: true
+        enumerable: true,
     },
     subjects: {
         value: [],
         writable: false,
         configurable: false,
-        enumerable: true
-    }
+        enumerable: true,
+    },
 })
 
 // student.subjects = ["Deutsch", "Englisch"]
-student.subjects.push("Deutsch")
+student.subjects.push('Deutsch')
 
 for (const key in student) {
-    console.log("key:", key)
+    console.log('key:', key)
 }
 // delete student.firstname
 // student.firstname = "Maximilian"
