84538c3b4c569c3d3f1b61ac57f415214f7c0da1
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-10 11:23:39 +0200

    First steps created

--- 26-API abfragen/Teil 02/index.js
+++ 26-API abfragen/Teil 02/index.js
@@ -1,17 +1,28 @@
+
 'use strict'
 
 const axios = require('axios').default
 
+/*
+axios.get("https://api.nal.usda.gov/fdc/v1/foods/search", {
+        params: {
+            api_key: "2foBawt5jmtipI7Hb4oyhj8fLZHOAJdjMatzM6Y5",
+            query: "Apple Juice"
+        }
+    })
+    .then(function(response) {
+        for (const food of response.data['foods']) {
+            console.log(food['fdcId'] + ": " + food['description']);
+        }
+    });
+*/
+
 axios
-    .get('https://api.nal.usda.gov/fdc/v1/foods/search', {
+    .get('https://api.nal.usda.gov/fdc/v1/food/1102747', {
         params: {
             api_key: '2foBawt5jmtipI7Hb4oyhj8fLZHOAJdjMatzM6Y5',
-            query: 'Apple Juice',
         },
     })
-    .then((response) => {
-        // console.log(response.data)
-        for (const food of response.data['foods']) {
-            console.log(food['fdcId'] + ': ' + food['description'])
-        }
+    .then(function (response) {
+        console.log(response.data)
     })
