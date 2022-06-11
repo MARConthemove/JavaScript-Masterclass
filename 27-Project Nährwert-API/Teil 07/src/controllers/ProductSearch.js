99c775c3e2c581d5ca52b5828af8557c5a4c4a55
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-11 16:46:56 +0200

    implemented product search

--- 27-Project Nährwert-API/Teil 07/src/controllers/ProductSearch.js
+++ 27-Project Nährwert-API/Teil 07/src/controllers/ProductSearch.js
@@ -1,64 +1,60 @@
-"use strict"
+'use strict'
 
-const EventEmitter = require("eventemitter3")
+const EventEmitter = require('eventemitter3')
 
-const {search} = require("../api/product")
-const {on} = require("../utils/dom")
-
+const { search } = require('../api/product')
+const { on } = require('../utils/dom')
 
 /**
- * 
- * @param {HTMLInputElement} inputElement 
- * @param {HTMLButtonElement} buttonElement 
+ *
+ * @param {HTMLInputElement} inputElement
+ * @param {HTMLButtonElement} buttonElement
  * @param {HTMLDivElement} resultElement
  */
 function ProductSearch(inputElement, buttonElement, resultElement) {
-  this.inputElement = inputElement
-  this.buttonElement = buttonElement
-  this.resultElement = resultElement
+    this.inputElement = inputElement
+    this.buttonElement = buttonElement
+    this.resultElement = resultElement
 
-  this.events = new EventEmitter()
+    this.events = new EventEmitter()
 }
 
 ProductSearch.prototype.init = function() {
-  this.buttonElement.addEventListener("click", (event) => {
-    event.preventDefault()
+    this.buttonElement.addEventListener('click', (event) => {
+        event.preventDefault()
 
-    const inputValue = this.inputElement.value
-    this.runSearch(inputValue)
-  })
+        const inputValue = this.inputElement.value
+        this.runSearch(inputValue)
+    })
 
-  on(".product-search--result-item", "click", (event) => {
-    event.originalEvent.preventDefault()
-    const fdcId = event.handleObj.getAttribute("data-fdcid")
-    
-    this.events.emit("productSelected", fdcId)
-  })
+    on('.product-search--result-item', 'click', (event) => {
+        event.originalEvent.preventDefault()
+        let fdcId = event.handleObj.getAttribute('data-fdcid')
+
+        this.events.emit("productSelected", fdcId)
+    })
 }
 
 /**
  * @param {String} term
  */
 ProductSearch.prototype.runSearch = function(term) {
-  search(term)
-    .then((results) => {
-      this.resultElement.innerHTML = ""
+    search(term).then((results) => {
+        this.resultElement.innerHTML = ''
 
-      for(const result of results) {
-        const linkElement = document.createElement("a")
-        linkElement.classList.add("list-group-item")
-        linkElement.classList.add("list-group-item-action")
-        linkElement.classList.add("product-search--result-item")
-        linkElement.setAttribute("href", "#")
-        linkElement.setAttribute("data-fdcid", result['fdcId'])
-        linkElement.innerText = result['description']
+        for (const result of results) {
+            const linkElement = document.createElement('a')
+            linkElement.classList.add('list-group-item')
+            linkElement.classList.add('list-group-item-action')
+            linkElement.classList.add('product-search--result-item')
+            linkElement.setAttribute('href', '#')
+            linkElement.setAttribute('data-fdcid', result['fdcId'])
+            linkElement.innerText = result['description']
 
-        this.resultElement.append(linkElement)
-      }
+            this.resultElement.append(linkElement)
+        }
 
-      console.log("results:", results)
-
-
+        console.log('results:', results)
     })
 }
 
