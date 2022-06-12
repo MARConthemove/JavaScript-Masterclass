ab5689f48ddd20d596eaaf402de263ae43cd1662
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-12 13:20:05 +0200

    ProductList controller implemented new features to calculate the fat, carbs and proteins of on nutrient, also calculates this for more than one nutrient

--- 27-Project Nährwert-API/Teil 07/src/controllers/ProductList.js
+++ 27-Project Nährwert-API/Teil 07/src/controllers/ProductList.js
@@ -3,6 +3,7 @@
 const { info } = require('../api/product')
 const { on } = require('../utils/dom')
 const addProductTemplate = require('../templates/ProductList/addProduct.ejs')
+const { EventEmitter } = require('eventemitter3')
 
 /** Constructor Function ProductList
  *
@@ -11,6 +12,8 @@ const addProductTemplate = require('../templates/ProductList/addProduct.ejs')
 function ProductList(listElement) {
     this.products = []
     this.listElement = listElement
+
+    this.events = new EventEmitter()
 }
 
 ProductList.prototype.init = function() {
@@ -26,6 +29,60 @@ ProductList.prototype.init = function() {
     })
 }
 
+// Funktion "füttert" eventEmitter mit dem nutrientChange Ereignis
+ProductList.prototype.emitNutrients = function() {
+    const nutrients = this.getNutrients()
+    this.events.emit("nutrientChange", nutrients)
+}
+
+ProductList.prototype.getNutrientsForProduct = function(product) {
+    const nutrients = {
+        carbs: 0,
+        protein: 0,
+        fat: 0
+    }
+
+    for (const foodNutrient of product.product.foodNutrients) {
+        if (("" + foodNutrient.nutrient.number) === "205") {
+            nutrients.carbs = foodNutrient.amount
+        } else if (("" + foodNutrient.nutrient.number) === "204") {
+            nutrients.fat = foodNutrient.amount
+        } else if (("" + foodNutrient.nutrient.number) === "203") {
+            nutrients.protein = foodNutrient.amount
+        }
+    }
+
+    return {
+        carbs: (nutrients.carbs / 100) * product.amount,
+        protein: (nutrients.protein / 100) * product.amount,
+        fat: (nutrients.fat / 100) * product.amount,
+
+    }
+    console.log("product: ", product)
+    console.log("nutrients: ", nutrients)
+    console.log('getNutrientsForProduct(): ', product)
+
+}
+
+ProductList.prototype.getNutrients = function() {
+    const nutrients = {
+        carbs: 0,
+        protein: 0,
+        fat: 0
+    }
+
+    for (const product of this.products) {
+        // Auslagerung der Funktion um Nährwerte für ein Produkt zu erhalten
+        const productNutrients = this.getNutrientsForProduct(product)
+
+        nutrients.carbs += productNutrients.carbs
+        nutrients.protein += productNutrients.protein
+        nutrients.fat += productNutrients.fat
+    }
+
+    return nutrients
+}
+
 ProductList.prototype.updateAmount = function(fdcId, value) {
     for (const product of this.products) {
         if (product.product["fdcId"] === fdcId) {
@@ -33,7 +90,8 @@ ProductList.prototype.updateAmount = function(fdcId, value) {
             break
         }
     }
-    console.log("updateAmount: ", this.products)
+    this.emitNutrients()
+    // console.log("updateAmount: ", this.products)
 }
 
 ProductList.prototype.removeProduct = function(fdcId) {
@@ -59,6 +117,10 @@ ProductList.prototype.removeProduct = function(fdcId) {
             trElement.remove()
         }
     }
+
+    this.emitNutrients()
+
+    // console.log("this.products: ", this.products)
 }
 
 ProductList.prototype.addProduct = function(fdcId) {
@@ -67,7 +129,7 @@ ProductList.prototype.addProduct = function(fdcId) {
             amount: 100,
             product: product,
         })
-        console.log("Products Array: ", this.products)
+
         const productHtml = addProductTemplate({
             title: product['description'],
             fdcId: product['fdcId'],
@@ -75,6 +137,10 @@ ProductList.prototype.addProduct = function(fdcId) {
 
         this.listElement.insertAdjacentHTML('beforeend', productHtml)
         // this.listElement.innerHTML = this.listElement.innerHTML + productHtml
+
+        // this.getNutrients()
+
+        this.emitNutrients()
     })
 }
 
