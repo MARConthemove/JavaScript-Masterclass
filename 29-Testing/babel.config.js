dbf9f864b133a39ac2db6e4b820366246a09f5f8
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-13 11:55:23 +0200

    Initializing Jest for testing

---
+++ 29-Testing/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: { node: 'current' },
+            },
+        ],
+    ],
+}
+
+// Sorgt dafür das alle modernen JavaScript Features sauber unterstützt werden
