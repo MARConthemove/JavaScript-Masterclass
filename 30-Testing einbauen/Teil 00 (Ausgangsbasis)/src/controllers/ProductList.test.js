ed684e522f3dfb24fa01eb5333c1f97dc750092f
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-14 11:51:12 +0200

    Testing ProductList Module start

---
+++ 30-Testing einbauen/Teil 00 (Ausgangsbasis)/src/controllers/ProductList.test.js
@@ -0,0 +1,12 @@
+"use strict"
+
+describe('ProductList', () => {
+  let productList, listElement
+
+  beforeEach(() => {
+    listElement = document.createElement("tbody")
+    productList = new ProductList(listElement)
+  });
+
+
+});
