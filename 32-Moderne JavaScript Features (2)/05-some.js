9d8dbe4958f3c0e04e28a13dade53f1ec470eae3
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-16 11:20:19 +0200

    ES6 .some()

---
+++ 32-Moderne JavaScript Features (2)/05-some.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const students = [{ firstname: 'Max' }, { firstname: 'Moritz' }]
+
+const exists = students.some(value => value.firstname === "Erik")
+
+console.log("exists: ", exists)
